115c82ffa81a53bcca4ece5968085a794ff5a6d1
kunasyl <kunasyl45@gmail.com>
2023-05-06 20:51:26 +0600

    added product models

--- e_store_project/settings.py
+++ e_store_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'  # данные откуда берем
+MEDIA_ROOT = BASE_DIR / 'media'  # данные куда загружаем
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
